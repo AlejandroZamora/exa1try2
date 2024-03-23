e9c3c206b7fb2659c9c83095da53267fbd3453c9
Alejandro <zamoraalejandro5@gmail.com>
2024-03-22 19:42:50 -0600

    con modelos.

---
+++ exa1try2/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exa1try2.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public int? QuantityPerUnit { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public int? UnitsInStock { get; set; }
+
+    public int? UnitsOnOrder { get; set; }
+
+    public int? ReorderLevel { get; set; }
+
+    public string? Discounted { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual Supplier? Supplier { get; set; }
+}
